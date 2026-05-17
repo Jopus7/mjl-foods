eee004921042310edc222752e880e2f781432a6e
Jopus7 <micha.jopa@gmail.com>
2026-05-17 20:28:53 +0200

    new product schemas

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -1,13 +1,25 @@
 from pydantic import BaseModel
 
+
 class ProductCreate(BaseModel):
     name: str
+    description: str
     price: float
+    image_url: str
+    available: bool = True
+    is_popular: bool = False
+    category: str
+
 
 class ProductResponse(BaseModel):
     id: int
     name: str
+    description: str
     price: float
+    image_url: str
+    available: bool
+    is_popular: bool
+    category: str
 
     class Config:
         from_attributes = True
