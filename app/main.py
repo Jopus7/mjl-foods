69aed45c4c8db91d13b5fd0b1f5d98fb98ba9efd
Jopus7 <micha.jopa@gmail.com>
2026-05-17 23:28:21 +0200

    added order feature

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.db.database import Base, engine
-from app.models import product
-from app.routes import products
+from app.models import product, order, order_item
+from app.routes import products, orders
 
 
 Base.metadata.create_all(bind=engine)
@@ -9,6 +9,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="MJL foods")
 
 app.include_router(products.router)
+app.include_router(orders.router)
 
 @app.get("/")
 def root():
---
+++ app/routes/orders.py
@@ -0,0 +1,159 @@
+from fastapi import (
+    APIRouter,
+    Depends,
+    HTTPException
+)
+
+from sqlalchemy.orm import Session
+
+from datetime import (
+    datetime,
+    timedelta
+)
+
+import uuid
+
+from app.db.database import get_db
+
+from app.models.order import Order
+from app.models.order_item import OrderItem
+from app.models.product import Product
+
+from app.schemas.order import (
+    OrderCreate,
+    OrderResponse
+)
+
+router = APIRouter()
+
+
+@router.get("/api/orders")
+def get_orders(
+    db: Session = Depends(get_db)
+):
+    return db.query(Order).all()
+
+
+@router.get("/api/orders/{order_id}")
+def get_order(
+    order_id: str,
+    db: Session = Depends(get_db)
+):
+    order = (
+        db.query(Order)
+        .filter(
+            Order.order_number == order_id
+        )
+        .first()
+    )
+
+    if not order:
+        raise HTTPException(
+            status_code=404,
+            detail="Order not found"
+        )
+
+    minutes_passed = (
+        datetime.now()
+        - order.created_at
+    ).total_seconds() / 60
+
+    if minutes_passed < 1:
+        status = "accepted"
+
+    elif minutes_passed < 2:
+        status = "preparing"
+
+    elif minutes_passed < 3:
+        status = "on_the_way"
+
+    else:
+        status = "delivered"
+
+    return {
+        "orderId": order.order_number,
+        "status": status,
+        "estimatedDeliveryTime":
+            order.estimated_delivery_time
+    }
+
+
+@router.post(
+    "/api/orders/create",
+    response_model=OrderResponse
+)
+def create_order(
+    order: OrderCreate,
+    db: Session = Depends(get_db)
+):
+    if not order.items:
+        raise HTTPException(
+            status_code=400,
+            detail="Order must contain items"
+        )
+
+    order_number = (
+        f"ORD-{uuid.uuid4().hex[:6].upper()}"
+    )
+
+    estimated_delivery = (
+        datetime.now()
+        + timedelta(minutes=45)
+    )
+
+    new_order = Order(
+        order_number=order_number,
+        first_name=order.customer.firstName,
+        last_name=order.customer.lastName,
+        phone=order.customer.phone,
+        email=order.customer.email,
+        address=order.customer.address,
+        postal_code=order.customer.postalCode,
+        city=order.customer.city,
+        comment=order.comment,
+        promo_code=order.promoCode,
+
+        status="accepted",
+
+        estimated_delivery_time=
+        estimated_delivery
+    )
+
+    db.add(new_order)
+    db.commit()
+    db.refresh(new_order)
+
+    for item in order.items:
+
+        product = (
+            db.query(Product)
+            .filter(
+                Product.id == item.productId
+            )
+            .first()
+        )
+
+        if not product:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Product {item.productId} not found"
+            )
+
+        order_item = OrderItem(
+            order_id=new_order.id,
+            product_id=product.id,
+            quantity=item.quantity,
+            price_at_purchase=product.price
+        )
+
+        db.add(order_item)
+
+    db.commit()
+
+    return {
+        "success": True,
+        "orderId": order_number,
+        "estimatedDeliveryTime":
+            estimated_delivery.isoformat(),
+        "status": "accepted"
+    }
