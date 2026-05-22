476a142d9fb0d349779c5172a886a13e49e3f9ee
Maja <majamaj2003@gmail.com>
2026-05-22 19:59:57 +0200

    fixed scroll issue

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
@@ -14,6 +15,7 @@ function App() {
   return (
     <CartProvider>
       <Router>
+        <ScrollToTop />
         <Navbar />
 
         <main className="content">
